a5168d93d7e9f3220d1556d8d59f29c59ac52879
Percy <percymc@outlook.com>
2017-04-27 00:25:26 -0500

    Change in frk55.sh

--- frk55.sh
+++ frk55.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A sample Bash script, by Percy MC
 # Copy from AsusK55v .emacs
-echo Copy from AsusK55v!
+echo Copy from AsusK55v!!
 cp ../.emacs asusk55.emacs.el
-echo By Percy_MC
+echo Copied ../.emacs to /asusk55.emacs.el
